5b06022e8b58829182f1c581939a78c0db2976d4
MaxxTorres <157403152+MaxxTorres@users.noreply.github.com>
2024-12-12 08:49:24 -0500

    removed amazon API key

--- shopping-app/src/api/amazon.js
+++ shopping-app/src/api/amazon.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: 'https://real-time-amazon-data.p.rapidapi.com',
     headers: {
       'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com',
-      'x-rapidapi-key': 'b17df51ccemsh1b9721718138f77p15a079jsnc168bb17ee62'
+      'x-rapidapi-key': 'DO NOT PUBLISH'
     }
   });
